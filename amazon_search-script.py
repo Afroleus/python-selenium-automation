5432eceb56649c27a2f94f705fd2ae66f3163f3e
Afroleus <lamebizkat@gmail.com>
2022-10-03 21:47:10 -0400

    hw2 updated!

--- amazon_search-script.py
+++ amazon_search-script.py
@@ -1,28 +1,30 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from time import sleep
 
 c = webdriver.ChromeOptions()
 c.add_argument("--incognito")
 
-driver = webdriver.Chrome(executable_path="./chromedriver.exe",options=c)
+driver = webdriver.Chrome(executable_path="./chromedriver.exe", options=c)
 driver.implicitly_wait(0.5)
+driver.maximize_window()
 
 driver.get('https://www.amazon.com/')
 
-driver.find_element(By.ID,"nav-orders").click()
+driver.find_element(By.ID, "nav-orders").click()
 
-expected_result_1 = driver.find_element(By.ID, "a-page")
-actual_result_1 = driver.find_element(By.ID,"a-page")
-assert expected_result_1 == actual_result_1, f'Error! Expected {expected_result_1}, but got {actual_result_1}'
-expected_result_2 = driver.find_element(By.ID,"authportal-main-section")
-actual_result_2 = driver.find_element(By.ID,"authportal-main-section")
+sleep(2)
+
+driver.find_element(By.ID, "a-page")
+
+expected_result_2 = 'Sign in'
+actual_result_2 = driver.find_element(By.XPATH, ".//h1").text
 assert expected_result_2 == actual_result_2, f'Error! Expected {expected_result_2}, but got {actual_result_2}'
-expected_result_3 = driver.find_element(By.ID,"ap_email")
-actual_result_3 = driver.find_element(By.ID,"ap_email")
-assert expected_result_3 == actual_result_3, f'Error! Expected {expected_result_3}, but got {actual_result_3}'
+
+
+driver.find_element(By.ID, "ap_email")
 
 print('Test Case Passed, browser closes immediately')
 driver.find_element(By.ID, "ap_email").send_keys('Browser will close in 10 seconds')
 driver.implicitly_wait(1)
 driver.quit()
-
---
+++ features/tests/amazon_search.feature
@@ -0,0 +1,6 @@
+# Created by NorkL at 10/1/2022
+Feature: Tests for amazon search
+  # Enter feature description here
+
+  Scenario: # Enter scenario name here
+    # Enter steps here
